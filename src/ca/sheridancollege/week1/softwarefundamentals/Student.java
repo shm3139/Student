340e60349a0750b3cd3978c8066e33e40a8f0db1
Man <shm@sheridancollege.ca>
2020-05-21 11:14:56 -0400

    Second commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -13,6 +13,16 @@ public class Student {
     
     private String name;
     
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public void setName(String name)
     {
         this.name=name;
